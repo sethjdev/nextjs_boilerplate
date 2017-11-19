cb1fb0e239a55d076eb6f9cab8f68c301bca7361
Seth Johnson <seth@lawnguru.co>
2017-11-19 16:01:23 -0500

    footer logo

--- components/footer.js
+++ components/footer.js
@@ -5,11 +5,15 @@ import Meta from './meta'
 
 export default () => (
     <div>
-        <footer className="pt-3 pb-3">
+        <footer className="pt-2 pb-2">
             <div class="container">
                 <div class="row d-flex align-items-center">
-                    <div class="col-6">
-                        <small>Website Inc.<br/>&copy; 2018 | All Rights Reserved</small>
+                    <div class="col-6 d-flex align-items-center justify-content-start">
+                        <img src="http://via.placeholder.com/100x100?text=logo" className="footer-logo mr-3" />
+                        <div>
+                            <small className="d-block">Website Inc.</small>
+                            <small className="d-block">&copy; 2018 | All Rights Reserved</small>
+                        </div>
                     </div>
                     <div class="col-6"></div>
                 </div>
@@ -23,6 +27,18 @@ export default () => (
                 bottom: 0;
                 left: 0;
                 background-color: white;
+
+                small {
+                    color: #a0a0a0;
+                }
+                
+                .footer-logo {
+                    width: 100%;
+                    max-width: 50px;
+                    height: auto;
+                    display: block;
+                    border-radius: 50px;
+                }
             }
         `}</style>
     </div>
